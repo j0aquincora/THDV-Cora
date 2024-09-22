efdc96a30c4a6c8178a4e9062b0501cd19b720bc
joaquin cora <joaquin.cora@estudiante.imagecampues.edu.ar>
2024-09-22 20:29:02 -0300

    Fix obstaculo giratorio

--- THDV-Cora/Assets/scrips/MysticRotationAndFloat.cs
+++ THDV-Cora/Assets/scrips/MysticRotationAndFloat.cs
@@ -2,19 +2,27 @@ using UnityEngine;
 
 public class MysticRotationAndFloat : MonoBehaviour
 {
-    public float rotationSpeed = 50f;       
+    [SerializeField]float rotationSpeed = 50;
     //public float floatMagnitude = 0.5f;     
     //public float floatSpeed = 1f;           
+    Rigidbody m_Rigidbody;
+    Vector3 m_EulerAngleVelocity;
 
-         
+    void Start()
+    {
+        //Fetch the Rigidbody from the GameObject with this script attached
+        m_Rigidbody = GetComponent<Rigidbody>();
 
+        //Set the angular velocity of the Rigidbody (rotating around the Y axis, 100 deg/sec)
+        m_EulerAngleVelocity = new Vector3(rotationSpeed, 0, 0);
+    }
     void Update()
     {
-        
-        transform.Rotate(Vector3.right   * rotationSpeed * Time.deltaTime);
 
-       
-        //Vector3 floatOffset = Vector3.up * Mathf.Sin(Time.time * floatSpeed) * floatMagnitude;
-        //transform.position = startPosition + floatOffset;
+        //transform.Rotate(Vector3.right   * rotationSpeed * Time.deltaTime);
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
+        m_Rigidbody.MoveRotation(m_Rigidbody.rotation * deltaRotation);
+
+
     }
 }
